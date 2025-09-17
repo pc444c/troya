40653c6033694d147a8e7176b52c43a128c19c6a
pc444c <tiktokonlytiktokonlytiktok@gmail.com>
2025-09-17 17:55:06 +0300

    Альфа версия 03

--- app/layouts/user.vue
+++ app/layouts/user.vue
@@ -24,7 +24,7 @@
         :class="buttonClass('/stats/all')"
         color="blue"
         class="shadow-lg px-4 sm:px-6 py-2 sm:py-3 rounded-full flex items-center justify-center w-full sm:w-auto"
-        @click="goTo('/stats')"
+        @click="goTo('/stats/all')"
       >
         <UIcon name="i-heroicons-chart-bar" class="w-5 h-5 mr-2" />
         Общая статистика
@@ -35,7 +35,7 @@
         :class="buttonClass('/stats')"
         color="blue"
         class="shadow-lg px-4 sm:px-6 py-2 sm:py-3 rounded-full flex items-center justify-center w-full sm:w-auto"
-        @click="goTo('/stats/all')"
+        @click="goTo('/stats')"
       >
         <UIcon name="i-heroicons-chart-pie" class="w-5 h-5 mr-2" />
         Моя статистика
